8bcc0997091013c1904165919bb8dd50b273adea
Egor KhromovE <khromovE@gmail.com>
2019-02-03 16:41:22 +0800

    Updates

---
+++ precache-manifest.ab634bf5987094ef71d6dbb335658b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f83583492949cb063ac",
+    "url": "/react-pokedex/static/js/main.8f835834.chunk.js"
+  },
+  {
+    "revision": "ad71f1b627e48324fdfe",
+    "url": "/react-pokedex/static/js/1.ad71f1b6.chunk.js"
+  },
+  {
+    "revision": "f9cd93de0834cc9f18e8",
+    "url": "/react-pokedex/static/js/runtime~main.f9cd93de.js"
+  },
+  {
+    "revision": "3d794d5b0e9beeb03e7168345d9fdc7d",
+    "url": "/react-pokedex/static/media/logo.3d794d5b.svg"
+  },
+  {
+    "revision": "e7d722b083feae04a0ef32480622ee40",
+    "url": "/react-pokedex/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2e3782b01c2ccc7f9ecc02a078e816d.js"
+  "/react-pokedex/precache-manifest.ab634bf5987094ef71d6dbb335658b42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-pokedex/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
