09ec1681cbb9f835c976b4437a992efeaacfa122
Egor KhromovE <khromovE@gmail.com>
2019-02-03 16:05:52 +0800

    Updates

---
+++ precache-manifest.d2e3782b01c2ccc7f9ecc02a078e816d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b30b2f7d8e766144820",
+    "url": "/static/js/main.9b30b2f7.chunk.js"
+  },
+  {
+    "revision": "cff457d1a527d1024cc0",
+    "url": "/static/js/1.cff457d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d794d5b0e9beeb03e7168345d9fdc7d",
+    "url": "/static/media/logo.3d794d5b.svg"
+  },
+  {
+    "revision": "72401603f41f93d3f6bac8c224263f52",
+    "url": "/index.html"
+  }
+];
